c2f3f01ec38b502ea710f9ff8128dceb64a9ccd5
Dima <shkoliarenkod@gmail.com>
2023-02-02 23:01:38 +0100

    Push

---
+++ Site/Site/Data/Models/Categories.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Data.Models
+{
+    public class Categories
+    {
+        public int id { get; set; }
+        public string categoryName { get; set; }
+        public string description { get; set; }
+        // public int MyProperty { get; set; }
+
+
+    }
+}
---
+++ Site/Site/Data/Models/bakeryProducts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Data.Models
+{
+    public class bakeryProducts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string  LongDescription { get; set; }
+        public string Image { get; set; }
+        public uint Price { get; set; }
+        public int IsAvaliable { get; set; }
+        public bool IsFavourite { get; set; }
+        public int CategoryID { get; set; }
+        public Categories Categories { get; set; }
+
+    }
+}
